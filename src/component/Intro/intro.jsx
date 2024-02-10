824e7e6e19c210c40204cafbb7d0bf72fa704569
NalakaDinesh <133525019+NalakaDineshSamarasingha@users.noreply.github.com>
2024-02-10 21:34:29 +0530

    first commit

---
+++ src/component/Intro/intro.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import './intro.css'
+import BG from '../../assests/my.png';
+import { Link } from "react-scroll";
+
+const Intro = () => {
+    return(
+        <div className="intro" id="intro">
+            <div className="introContent">
+                <span className="hello">Hello,</span>
+                <span className="introText">I'm <span className="introName">Nalaka</span> <br />Web Developer </span>
+                <p className="introPara">
+                    I am a skilled web developer with experience in creating <br />visually appealing and friendly website  
+                </p>
+                <Link><button className="btn1">&#9874; Hire me</button></Link>
+            </div>
+            <img src={BG} alt="Bg" className="bg"  />
+        </div>
+    )
+}
+export default Intro;
---
+++ src/component/Navbar/nav.jsx
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+import './nav.css'
+import {Link} from 'react-scroll'
+
+const Navbar = () =>{
+    const [showMenu,setShowMenu] = useState(false);
+    return(
+        <div className="navbar">
+            <p className="nav-logo">Nalaka<span>.me</span></p>
+            <div className="navmenu">
+                <Link className="desktopMenuListItem">Home</Link>
+                <Link className="desktopMenuListItem">About</Link>
+                <Link className="desktopMenuListItem">Portfolio</Link>
+                <Link className="desktopMenuListItem">Client</Link>
+            </div>
+            <button className="nav-btn">
+               <span>&#9993; </span>  Contact me
+            </button>
+
+            <p className="mobo" onClick={()=> setShowMenu(!showMenu)}>&#9776;</p>
+            <div className="mobomenu"  style={{display: showMenu?'flex':'none'}}>
+                <Link className="listItem" onClick={()=>setShowMenu(false)} >Home</Link>
+                <Link className="listItem" onClick={()=>setShowMenu(false)} >About</Link>
+                <Link className="listItem" onClick={()=>setShowMenu(false)} >Portfolio</Link>
+                <Link className="listItem" onClick={()=>setShowMenu(false)} >Client</Link>
+                <Link className="listItem" onClick={()=>setShowMenu(false)} >Contact</Link>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar;
