d53f6a4b818e2297cf9d3d5c7a8bb168d4bc0b55
NalakaDinesh <133525019+NalakaDineshSamarasingha@users.noreply.github.com>
2024-02-15 20:20:20 +0530

    setup the emailJS severver

--- src/component/contact/contact.jsx
+++ src/component/contact/contact.jsx
@@ -1,22 +1,35 @@
-import React from "react";
+import React, { useRef } from "react";
 import './contact.css';
 import Facebook from '../../assests/fb.png';
 import Instagram from '../../assests/in.png';
 import Linkdin from '../../assests/link.png';
 import Github from '../../assests/git.png';
+import emailjs from '@emailjs/browser';
 
 const Contact = ()=>{
+    const form = useRef();
+    const sendEmail = (e) => {
+        e.preventDefault();
+        emailjs.sendForm('service_usjgxrj', 'template_0sfw6b7', form.current, {
+            publicKey: 'fDJ1iQS_P9SHT9KRV',}).then(() => {
+                console.log('SUCCESS!');
+                alert("Mail send");
+            },
+            (error) => {
+                console.log('FAILED...', error.text);
+                alert("Mail send failed");
+            },
+        );
+    };
     return(
         <div className="contactPage">
-
-        
         <div id="contact">
             <h1 className="contactPageTitle">Contact Me</h1>
             <span id="contactDes">Please fill out the form below to discuss any work opportunities</span>
-            <form className="contactForm">
-                <input type="text" className="name" placeholder="Your Name" />
-                <input type="email" className="email" placeholder="Your Email" />
-                <textarea name="message" rows="5" className="massage" placeholder="Your Message"></textarea>
+            <form className="contactForm" ref={form} onSubmit={sendEmail}>
+                <input type="text" className="name" placeholder="Your Name" name="from_name"/>
+                <input type="email" className="email" placeholder="Your Email" name="from_email" />
+                <textarea name="message" rows="5" className="massage" placeholder="Your Message" ></textarea>
                 <button className="submitbtn" type="submit" value="send">Submit</button>
                 <div className="links">
                     <img src={Facebook} alt="Facebook" style={{width:"30px"}} className="link"/>
