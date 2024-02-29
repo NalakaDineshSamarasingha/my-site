9615fafdd626b7884c967ea61970ad06351f270a
NalakaDinesh <133525019+NalakaDineshSamarasingha@users.noreply.github.com>
2024-02-29 12:51:00 +0530

    add animation skills section

--- src/component/contact/contact.jsx
+++ src/component/contact/contact.jsx
@@ -1,10 +1,12 @@
 import React, { useRef } from "react";
-import './contact.css';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import './contact.scss';
 import Facebook from '../../assests/fb.png';
 import Instagram from '../../assests/in.png';
 import Linkdin from '../../assests/link.png';
 import Github from '../../assests/git.png';
 import emailjs from '@emailjs/browser';
+import { faCss3, faGitAlt, faHtml5,  faJsSquare, faNodeJs, faReact } from "@fortawesome/free-brands-svg-icons";
 
 const Contact = ()=>{
     const form = useRef();
@@ -31,7 +33,28 @@ const Contact = ()=>{
                 These are few example for that.
             </p>
             <div className="skill-container">
-
+                <div className="stage-cube-cont">
+                    <div className="cubespinner">
+                        <div className="face1">
+                            <FontAwesomeIcon icon={faReact} color="#61dbfb"/>
+                        </div>
+                        <div className="face2">
+                            <FontAwesomeIcon icon={faHtml5} color="#f06529"/>
+                        </div>
+                        <div className="face3">
+                            <FontAwesomeIcon icon={faCss3} color="#28a4d9"/>
+                        </div>
+                        <div className="face4">
+                            <FontAwesomeIcon icon={faJsSquare} color="#efd81d"/>
+                        </div>
+                        <div className="face5">
+                            <FontAwesomeIcon icon={faNodeJs} color="#61dbfb"/>
+                        </div>
+                        <div className="face6">
+                            <FontAwesomeIcon icon={faGitAlt} color="#ec4d28"/>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
         <div id="contact">
