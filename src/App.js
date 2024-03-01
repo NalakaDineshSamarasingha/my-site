310ab3907a7986744b2fd86befcac2a072691aa7
NalakaDinesh <133525019+NalakaDineshSamarasingha@users.noreply.github.com>
2024-03-01 21:35:45 +0530

    add loading effect

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import {useState,useEffect} from 'react';
 import './App.css';
 import Navbar from './component/Navbar/nav';
 import Intro from './component/Intro/intro';
@@ -5,16 +6,34 @@ import Skill from './component/skills/skills';
 import Footer from './component/footer/footer';
 import Contact from './component/contact/contact';
 import Portfolio from './component/portfolio/portfolio';
+import ClipLoader from "react-spinners/GridLoader";
 
 function App() {
+  const [loading,setLoading] = useState(false);
+  useEffect(()=>{
+    setLoading(true);
+    setTimeout(()=>{
+      setLoading(false);
+    },5000)
+  },[])
   return (
     <div className="App">
-      <Navbar/>
-      <Intro/>
-      <Skill/>
-      <Portfolio/>
-      <Contact/>
-      <Footer/>
+      {
+        loading?
+        <div className='loader'>
+          <ClipLoader color={"#700000"} loading={loading} size={20} aria-label="Loading Spinner" data-testid="loader"/>
+        </div>
+        :
+        <>
+          <Navbar/>
+          <Intro/>
+          <Skill/>
+          <Portfolio/>
+          <Contact/>
+          <Footer/>
+        </>
+      }
+
     </div>
   );
 }
